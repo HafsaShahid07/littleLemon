a70e3909d4afd5048fb780a3c96fcdd0837372f3
Hafsa Shahid <hafsashahidchaudhry@gmail.com>
2025-06-20 10:54:40 +0500

    Added all the menu items

--- workplace/littlelemon/restaurant/admin.py
+++ workplace/littlelemon/restaurant/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Menu
+from .models import Booking
+
+
 admin.site.register(Menu)
--- workplace/littlelemon/restaurant/models.py
+++ workplace/littlelemon/restaurant/models.py
@@ -13,9 +13,12 @@ class Booking(models.Model):
 
 
 # Add code to create Menu model
+from django.db import models
+
 class Menu(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
+    menu_item_description = models.TextField(max_length=1000, default='')
 
     def __str__(self):
         return self.name
